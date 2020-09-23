fd1bf92ad7babc15a651d0ba81d3bd11ba9776b8
adithyaanilkumar <adithyaanilkumar1@gmail.com>
2020-09-23 21:56:27 +0530

    added readme for server

--- server/models/user.js
+++ server/models/user.js
@@ -4,7 +4,10 @@ const UserSchema = new mongoose.Schema({
     username: {type:String, unique: true, required: true},
     password: {type:String, required: true},
     email: {type:String, unique: true, required: true},
+    phone: {type:Number, unique: true, required: true}, 
     photo: {type:String,  default: null},
+    blooodGroup: {type:String,  default: null},
+    raltivePhone:  {type:Number, unique: true, required: true}
 })
 
 module.exports = mongoose.model('User',UserSchema);
