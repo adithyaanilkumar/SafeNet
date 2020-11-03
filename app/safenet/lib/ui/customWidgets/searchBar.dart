4b626232f6fa1b7fc924b59f640c131d0a85d721
Sriram Patibanda <patibandasriram2k02@gmail.com>
2020-11-04 03:04:21 +0530

    bug fixes
    Adjusted search bar so that it floats on the map, fixed the bug where accurate location wasn't shown

--- app/safenet/lib/ui/customWidgets/searchBar.dart
+++ app/safenet/lib/ui/customWidgets/searchBar.dart
@@ -1,25 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:safenet/ui/constants.dart';
 
 class SearchBar extends StatelessWidget {
-  const SearchBar({
-    Key key,
-  }) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: Card(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.all(Radius.circular(10.0)),
-        ),
-        child: Container(
-          width: 385.0,
-          child: TextField(
-            decoration: kTextFieldDecoration.copyWith(
-                hintText: 'Search',
-                prefixIcon: Icon(Icons.arrow_back_ios),
-                suffixIcon: Icon(Icons.search_rounded)),
-          ),
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(Radius.circular(10.0)),
+      ),
+      child: Container(
+        width: 385.0,
+        child: TextField(
+          decoration: kTextFieldDecoration.copyWith(
+              hintText: 'Search',
+              prefixIcon: Icon(Icons.arrow_back_ios),
+              suffixIcon: Icon(Icons.search_rounded)),
         ),
       ),
     );
--- app/safenet/lib/ui/views/home_view.dart
+++ app/safenet/lib/ui/views/home_view.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:safenet/ui/constants.dart';
-import 'package:geolocation/geolocation.dart';
+import 'package:floating_search_bar/floating_search_bar.dart';
+import 'package:safenet/core/services/location.dart';
 import 'package:flutter_map/flutter_map.dart';
 import "package:latlong/latlong.dart" as latLng;
+import 'package:safenet/ui/constants.dart';
 import 'package:safenet/ui/customWidgets/searchBar.dart';
 
 class HomeView extends StatefulWidget {
@@ -12,73 +13,109 @@ class HomeView extends StatefulWidget {
 
 class _HomeViewState extends State<HomeView> {
   MapController controller = MapController();
-
-  getPermission() async {
-    final GeolocationResult result =
-        await Geolocation.requestLocationPermission(
-            permission: const LocationPermission(
-                android: LocationPermissionAndroid.fine));
-    return result;
-  }
-
-  getLocation() async {
-    return getPermission().then((result) async {
-      if (result.isSuccessful) {
-        final coords =
-            await Geolocation.currentLocation(accuracy: LocationAccuracy.city);
-        return coords;
-      }
-    });
-  }
+  Location location = Location();
 
   buildMap() {
-    getLocation().then((response) {
-      if (response.isSuccessful) {
-        response.listen((value) {
-          controller.move(
-              latLng.LatLng(value.location.latitude, value.location.longitude),
-              15.0);
-        });
-      }
+    location.getCurrentLocation().then((response) {
+      controller.move(
+          latLng.LatLng(location.latitude, location.longitude), 15.0);
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        // extendBodyBehindAppBar: true,
-        body: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 15.0),
-          child: Column(
-            children: [
-              Row(
-                children: [
-                  SearchBar(),
-                ],
-              ),
-              Expanded(
-                child: FlutterMap(
-                  mapController: controller,
-                  options: MapOptions(center: buildMap(), minZoom: 5.0),
-                  layers: [
-                    TileLayerOptions(
-                        urlTemplate:
-                            "https://api.mapbox.com/styles/v1/shrinkingviolet/ckgv04qs30mur19p91h9euprb/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoic2hyaW5raW5ndmlvbGV0IiwiYSI6ImNrZ2h0NnE3NDFka3Iyc25heGxudWpyNGEifQ.jY8M7bZoK5WxWuF81lNVhQ",
-                        additionalOptions: {
-                          'accessToken':
-                              'pk.eyJ1Ijoic2hyaW5raW5ndmlvbGV0IiwiYSI6ImNrZ2h0NnE3NDFka3Iyc25heGxudWpyNGEifQ.jY8M7bZoK5WxWuF81lNVhQ',
-                          'id': 'mapbox.mapbox-streets-v8'
-                        }),
-                  ],
-                ),
-              )
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          FlutterMap(
+            mapController: controller,
+            options: MapOptions(center: buildMap(), minZoom: 5.0),
+            layers: [
+              TileLayerOptions(
+                  urlTemplate:
+                      "https://api.mapbox.com/styles/v1/shrinkingviolet/ckgv04qs30mur19p91h9euprb/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoic2hyaW5raW5ndmlvbGV0IiwiYSI6ImNrZ2h0NnE3NDFka3Iyc25heGxudWpyNGEifQ.jY8M7bZoK5WxWuF81lNVhQ",
+                  additionalOptions: {
+                    'accessToken':
+                        'pk.eyJ1Ijoic2hyaW5raW5ndmlvbGV0IiwiYSI6ImNrZ2h0NnE3NDFka3Iyc25heGxudWpyNGEifQ.jY8M7bZoK5WxWuF81lNVhQ',
+                    'id': 'mapbox.mapbox-streets-v8'
+                  }),
             ],
           ),
-        ),
+          Positioned(
+              top: 10,
+              right: 15,
+              left: 15,
+              child: Container(
+                child: Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                  ),
+                  child: Container(
+                    width: 385.0,
+                    child: TextField(
+                      decoration: kTextFieldDecoration.copyWith(
+                          hintText: 'Search',
+                          prefixIcon: Icon(Icons.arrow_back_ios),
+                          suffixIcon: Icon(Icons.search_rounded)),
+                    ),
+                  ),
+                ),
+              )),
+        ],
       ),
     );
   }
+
+  // Widget searchbar() {
+  //   final isPortrait =
+  //       MediaQuery.of(context).orientation == Orientation.portrait;
+  //   return FloatingSearchBar(
+  //     hint: 'Search',
+  //     scrollPadding: const EdgeInsets.only(top: 16, bottom: 56),
+  //     transitionDuration: const Duration(milliseconds: 800),
+  //     transitionCurve: Curves.easeInOut,
+  //     physics: const BouncingScrollPhysics(),
+  //     axisAlignment: isPortrait ? 0.0 : -1.0,
+  //     openAxisAlignment: 0.0,
+  //     maxWidth: isPortrait ? 600 : 500,
+  //     debounceDelay: const Duration(milliseconds: 500),
+  //     onQueryChanged: (query) {
+  //       // Call your model, bloc, controller here.
+  //     },
+  //     // Specify a custom transition to be used for
+  //     // animating between opened and closed stated.
+  //     transition: CircularFloatingSearchBarTransition(),
+  //     actions: [
+  //       FloatingSearchBarAction(
+  //         showIfOpened: false,
+  //         child: CircularButton(
+  //           icon: const Icon(Icons.place),
+  //           onPressed: () {},
+  //         ),
+  //       ),
+  //       FloatingSearchBarAction.searchToClear(
+  //         showIfClosed: false,
+  //       ),
+  //     ],
+  //     builder: (context, transition) {
+  //       return ClipRRect(
+  //         borderRadius: BorderRadius.circular(8),
+  //         child: Material(
+  //           color: Colors.white,
+  //           elevation: 4.0,
+  //           child: Column(
+  //             mainAxisSize: MainAxisSize.min,
+  //             children: Colors.accents.map((color) {
+  //               return Container(height: 112, color: color);
+  //             }).toList(),
+  //           ),
+  //         ),
+  //       );
+  //     },
+  //   );
+  // }
 }
 
 // Color(0xff0278ae)
