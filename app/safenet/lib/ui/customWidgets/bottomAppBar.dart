a4f2d922aa8cd66f13002757c952ef3f8f5c07bf
Sriram Patibanda <patibandasriram2k02@gmail.com>
2020-11-04 16:38:51 +0530

    tweaked bottomAppBar

--- app/safenet/lib/ui/customWidgets/bottomAppBar.dart
+++ app/safenet/lib/ui/customWidgets/bottomAppBar.dart
@@ -29,42 +29,48 @@ class _CustomBottomAppBarState extends State<CustomBottomAppBar> {
           elevation: 2.0,
           backgroundColor: Colors.amber,
         ),
-        bottomNavigationBar: BottomAppBar(
-          shape: CircularNotchedRectangle(),
-          clipBehavior: Clip.antiAlias,
-          notchMargin: 5.0,
-          child: Container(
-            height: 75.0,
-            child: Row(
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                IconButton(
-                    icon: Icon(
-                      Icons.home,
-                      size: 40.0,
-                      color: Color(0xffe8ffc1),
-                    ),
-                    onPressed: () {
-                      setState(() {
-                        _controller.jumpToPage(0);
-                      });
-                    }),
-                IconButton(
-                    icon: Icon(
-                      Icons.account_circle,
-                      size: 40.0,
-                      color: Color(0xffe8ffc1),
-                    ),
-                    onPressed: () {
-                      setState(() {
-                        _controller.jumpToPage(1);
-                      });
-                    })
-              ],
-            ),
+        bottomNavigationBar: ClipRRect(
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(35),
+            topRight: Radius.circular(35),
           ),
-          color: Color(0xff51adcf),
+          child: BottomAppBar(
+            shape: CircularNotchedRectangle(),
+            clipBehavior: Clip.antiAlias,
+            notchMargin: 5.0,
+            child: Container(
+              height: 65.0,
+              child: Row(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  IconButton(
+                      icon: Icon(
+                        Icons.home,
+                        size: 40.0,
+                        color: Color(0xffe8ffc1),
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          _controller.jumpToPage(0);
+                        });
+                      }),
+                  IconButton(
+                      icon: Icon(
+                        Icons.account_circle,
+                        size: 40.0,
+                        color: Color(0xffe8ffc1),
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          _controller.jumpToPage(1);
+                        });
+                      })
+                ],
+              ),
+            ),
+            color: Color(0xff51adcf),
+          ),
         ),
         body: PageView(
           physics: new NeverScrollableScrollPhysics(),
