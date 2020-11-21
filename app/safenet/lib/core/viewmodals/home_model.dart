d3a79b181aee8764843e4f982b607f997c9c9374
Sriram Patibanda <patibandasriram2k02@gmail.com>
2020-11-21 21:12:44 +0530

    added markers succesfully

--- app/safenet/lib/core/viewmodals/home_model.dart
+++ app/safenet/lib/core/viewmodals/home_model.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import "package:latlong/latlong.dart" as latLng;
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class HomeViewModel extends ChangeNotifier {
   int _currentIndex = 0;
@@ -12,4 +15,36 @@ class HomeViewModel extends ChangeNotifier {
 
   double latitude;
   double longitude;
+
+  List<Marker> markers = [
+    Marker(
+      width: 80.0,
+      height: 80.0,
+      point: latLng.LatLng(51.5, -0.09),
+      builder: (context) => Container(
+        child: Icon(
+          FontAwesomeIcons.mapMarker,
+          color: Colors.red,
+          size: 25.0,
+        ),
+      ),
+    ),
+  ];
+
+  addMarker(double lat, double long) {
+    var marker = Marker(
+        width: 80.0,
+        height: 80.0,
+        point: latLng.LatLng(lat, long),
+        builder: (context) => Container(
+              child: Icon(
+                FontAwesomeIcons.mapMarker,
+                color: Colors.red,
+                size: 25.0,
+              ),
+            ));
+    markers.clear();
+    markers.add(marker);
+    notifyListeners();
+  }
 }
